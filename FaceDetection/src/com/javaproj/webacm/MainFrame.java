63fe3935d406fc5f584c497bb90f4e1a75025b1d
AKHIL SHARMA <supercellakhil75@gmail.com>
2021-08-03 14:10:31 +0530

    Add files via upload

---
+++ FaceDetection/src/com/javaproj/webacm/MainFrame.java
@@ -0,0 +1,52 @@
+package com.javaproj.webacm;
+
+import javax.swing.JFrame;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.videoio.VideoCapture;
+
+public class MainFrame extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private Detector detector;
+	private CameraPanel cameraPanel;
+	
+	public MainFrame() {
+		super("Face Detection");
+		
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		
+		detector = new Detector();
+		cameraPanel = new CameraPanel();
+		
+		setContentPane(cameraPanel);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(600, 500);
+		setVisible(true);
+	}
+		
+	public void displayScreen() {
+		Mat webcamImage = new Mat();
+		VideoCapture videoCapture = new VideoCapture(0);
+		
+		if (videoCapture.isOpened()) {
+			
+			while (true) {
+				
+				videoCapture.read(webcamImage);
+				
+				if (!webcamImage.empty()) {
+					setSize(webcamImage.width() + 50, webcamImage.height() + 70);
+					webcamImage = detector.detect(webcamImage);
+					cameraPanel.convertMattoImage(webcamImage);
+					cameraPanel.repaint();
+				} else {
+					System.out.println("Problem");
+					break;
+				}
+			}
+		}
+	}
+}
